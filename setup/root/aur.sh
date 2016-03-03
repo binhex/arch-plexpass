f82fcfd303433a82042d4ef97bd14685e43e0bfb
binhex <megalith01@gmail.com>
2016-03-03 09:38:54 +0000

    switch to apacman

---
+++ setup/root/aur.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# exit script if return code != 0
+set -e
+
+# define aur helper
+aur_helper="apacman"
+
+# define pacman packages
+pacman_packages="base-devel systemd"
+
+# define packer packages
+packer_packages="plex-media-server-plexpass"
+
+# install required pre-reqs for makepkg
+pacman -S --needed $pacman_packages --noconfirm
+
+# create "makepkg-user" user for makepkg
+useradd -m -s /bin/bash makepkg-user
+echo -e "makepkg-password\nmakepkg-password" | passwd makepkg-user
+echo "makepkg-user ALL=(ALL) NOPASSWD: ALL" | (EDITOR="tee -a" visudo)
+
+# download aur helper
+curl -o "/home/makepkg-user/$aur_helper.tar.gz" "https://aur.archlinux.org/cgit/aur.git/snapshot/$aur_helper.tar.gz"
+cd /home/makepkg-user
+su -c "tar -xvf $aur_helper.tar.gz" - makepkg-user
+
+# install aur helper
+su -c "cd /home/makepkg-user/$aur_helper && makepkg -s --noconfirm --needed" - makepkg-user
+pacman -U "/home/makepkg-user/$aur_helper/$aur_helper*.tar.xz" --noconfirm
+
+# install app using aur helper
+su -c "$aur_helper -S $aur_packages --noconfirm" - makepkg-user
+
+# remove base devel tools
+pacman -Ru base-devel git --noconfirm
+
+# delete makepkg-user account
+userdel -r makepkg-user
--- setup/root/install.sh
+++ setup/root/install.sh
@@ -3,8 +3,8 @@
 # exit script if return code != 0
 set -e
 
-# call aur packer script
-source /root/packer.sh
+# call aur install script
+source /root/aur.sh
 
 # cleanup
 yes|pacman -Scc
