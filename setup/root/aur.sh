e0da335c50c98ab2c23cf9b499cdf1b9e9bdafce
binhex <megalith01@gmail.com>
2016-03-03 11:09:27 +0000

    add in proper install for aur helper, for when its fixed

--- setup/root/aur.sh
+++ setup/root/aur.sh
@@ -20,9 +20,18 @@ useradd -m -s /bin/bash makepkg-user
 echo -e "makepkg-password\nmakepkg-password" | passwd makepkg-user
 echo "makepkg-user ALL=(ALL) NOPASSWD: ALL" | (EDITOR="tee -a" visudo)
 
-# download aur helper
+# download aur helper (patched version for rpc v5)
 curl -L -o "/usr/bin/$aur_helper" "https://github.com/binhex/arch-patches/raw/master/arch-packer/$aur_packages"
 chmod a+x "/usr/bin/$aur_packages"
+
+# download aur helper
+# curl -L -o "/home/makepkg-user/$aur_helper.tar.gz" "https://aur.archlinux.org/cgit/aur.git/snapshot/$aur_helper.tar.gz"
+# cd /home/makepkg-user
+# su -c "tar -xvf $aur_helper.tar.gz" - makepkg-user
+
+# install aur helper
+# su -c "cd /home/makepkg-user/$aur_helper && makepkg -s --noconfirm --needed" - makepkg-user
+# pacman -U "/home/makepkg-user/$aur_helper/$aur_helper*.tar.xz" --noconfirm
 
 # install app using aur helper
 su -c "$aur_helper -S $aur_packages --noconfirm" - makepkg-user
