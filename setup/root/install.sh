686827627508dcb5dd1b23b7d7366a5dbc067f5f
binhex <megalith01@gmail.com>
2016-03-15 15:04:44 +0000

    prevent removal of core packages when removing base-devel

--- setup/root/install.sh
+++ setup/root/install.sh
@@ -3,7 +3,13 @@
 # exit script if return code != 0
 set -e
 
-# call aur install script
+# define pacman packages
+pacman_packages="base-devel git systemd"
+
+# install required pre-reqs for makepkg
+pacman -S --needed $pacman_packages --noconfirm
+
+# call aur install script (arch user repo)
 source /root/aur.sh
 
 # cleanup
