7b80bcc5136213d03b92a6be648680d31aab6952
binhex <megalith01@gmail.com>
2016-06-11 20:40:26 +0100

    update aur

--- setup/root/aur.sh
+++ setup/root/aur.sh
@@ -16,9 +16,12 @@ echo "makepkg-user ALL=(ALL) NOPASSWD: ALL" | (EDITOR="tee -a" visudo)
 
 # download aur helper
 curl -L -o "/home/makepkg-user/$aur_helper.tar.gz" "https://aur.archlinux.org/cgit/aur.git/snapshot/$aur_helper.tar.gz"
+cd /home/makepkg-user
 su -c "tar -xvf $aur_helper.tar.gz" - makepkg-user
 
 # install aur helper
+su -c "cd /home/makepkg-user/$aur_helper && makepkg -s --noconfirm --needed" - makepkg-user
+pacman -U /home/makepkg-user/$aur_helper/packer*.pkg.tar.xz --noconfirm
 
 # install app using aur helper
 su -c "$aur_helper -S $aur_packages --noconfirm" - makepkg-user
