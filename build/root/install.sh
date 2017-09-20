2107b49e13b345748a67063a7f635655c0f71f30
binhex <megalith01@gmail.com>
2017-09-20 14:57:37 +0100

    need to specify libvpx as well for ffmpeg

--- build/root/install.sh
+++ build/root/install.sh
@@ -19,7 +19,7 @@ mv /tmp/scripts-master/shell/arch/docker/*.sh /root/
 ####
 
 # define pacman packages
-pacman_packages="git systemd libx264"
+pacman_packages="git systemd libx264 libvpx"
 
 # install compiled packages using pacman
 if [[ ! -z "${pacman_packages}" ]]; then
