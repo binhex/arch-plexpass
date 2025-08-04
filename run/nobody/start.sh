fcaa4158df545dd9c305799b97460f4ded88dcab
binhex <megalith01@gmail.com>
2025-08-04 09:13:53 +0100

    put in waitproc

--- run/nobody/start.sh
+++ run/nobody/start.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/dumb-init /bin/bash
 
+# source in script to wait for child processes to exit
+source waitproc.sh
+
 # config below is a consolidation of (original) bash script /usr/bin/plexmediaserver.sh and environment file /etc/conf.d/plexmediaserver
 
 # set env variables for plex
